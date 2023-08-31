574510166a03d3a319ee91fcb52286269937e642
agrfhyl <andyl2026@student.cis.edu.hk>
2023-08-31 14:37:40 +0800

    Wrote required cote

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to dating age calculator!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age (please enter an integer): ");
+        int Age = scanner.nextInt();
+        int Date = 7 + Age/2;
+        System.out.println("Your date should be at least " + Date + " years old!");
+
         
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        System.out.println("Wanna know how many pages you should read before giving up on a book?" + "\n" + "If so, you are in the right place!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age (an integer please): ");
+        int age = scanner.nextInt();
+        int pages = 100 - age;
+        System.out.println("Since you are " + age + " years old, " + "\n" + "You should read a minimum of " + pages + " pages before giving up on a book!" );
     }
 }
